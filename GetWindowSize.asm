f86b61ec4a821400180294422940d6c74f64bd7a
Roman <romanstrah@mail.ru>
2020-06-23 16:03:21 +0300

    Update GetWindowSize.asm

--- GetWindowSize.asm
+++ GetWindowSize.asm
@@ -1,30 +1,37 @@
+; Все необходимые прототипы процедур и библиотеки находятся тут.
 INCLUDE Irvine32.inc
 
-; В eax - ширина, в ebx - высота.
-
 .data
-	buffInfo CONSOLE_SCREEN_BUFFER_INFO <>
+	bufferInfo CONSOLE_SCREEN_BUFFER_INFO <>    ; Необходимо для хранения промежуточных данных.
 	
 .code
 main PROC
+	call GetWindowSize
+	exit
+main ENDP
+
+; Процедура получения размеров окна консоли. Не путать окно и буффер!
+; Результат: в ax - ширина, в bx - высота окна.
+GetWindowSize PROC
 
 	; Получаем в eax дескриптор стандартного устройства вывода.
+	; Результат заносится в eax.
 	INVOKE GetStdHandle, STD_OUTPUT_HANDLE
 
 	; Получаем информацию о текущем состоянии окна терминала.
-	INVOKE GetConsoleScreenBufferInfo, eax, ADDR buffInfo
+	INVOKE GetConsoleScreenBufferInfo, eax, ADDR bufferInfo
 
 	; Вычисляем ширину окна.
-	movzx eax, buffInfo.srWindow.Right
-	sub ax, buffInfo.srWindow.Left
+	mov ax, bufferInfo.srWindow.Right
+	sub ax, bufferInfo.srWindow.Left
 	add ax, 1
 	
 	; Вычисляем высоту окна.
-	movzx ebx, buffInfo.srWindow.Bottom
-	sub bx, buffInfo.srWindow.Top
+	mov bx, bufferInfo.srWindow.Bottom
+	sub bx, bufferInfo.srWindow.Top
 	add bx, 1
 	
-	call DumpRegs
-	exit
-main ENDP
+	ret
+GetWindowSize ENDP
+
 END main
